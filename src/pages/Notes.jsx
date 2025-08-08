09e9b2338863b80ca6972e3b1f30e553799e21d9
piegowatapanna <klaudia.frankowska.lodz@gmail.com>
2025-08-08 18:27:09 +0200

    Dodanie podstawowej struktury komponentu Notes z obsługą wpisywanego tekstu

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
-import './Notes.css';
+import React, { useState }from 'react';
 
-const Notes = () => {
+//Komponent notes - w tej wersji posiada tylko pole do wpisania tekstu i przycisk Dodaj notatke
+export default function Notes () {
+
+  //Stan komponentu do przechowywania wpisywanego tekstu w polu input
+  //noteText - aktualna wartość pola
+  //setNoteText - fukncja do zmiany tej wartości
+  const [noteText, setNoteText] = useState('');
+
   return (
-    <div className='notes-container'>
-      <h1 className='notes-title'>Notes</h1>
-    </div>
-  )
-}
+    <div>
+      {/*Tytuł podstrony */}
+      <h1>Notes</h1>
 
-export default Notes
+      {/* Formularz dodawania notatki, e.preventDefault - blokuje domyślnie przeładowania strony po wysłaniu formularza*/}
+      <form onSubmit={(e) => e.preventDefault()}>
+        {/* Pole tekstowe kontrolowane przez React (wartość pochodzi ze stanu noteText) */}
+        <input
+        type="text"
+        placeholder='Wpisz notatkę...'
+        value={noteText} //Wartość pola jest zawsze równa stanowi noteText
+        onChange={(e) => setNoteText(e.target.value)} //Kada zmiana w polu aktualizuje stan
+        />
+         {/* Przycisk do wysyłania formularza */}
+        <button type='submit'>Dodaj notatkę</button>
+      </form>
+      </div>
+  );
+}
