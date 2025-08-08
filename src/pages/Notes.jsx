fe658f5a773a347d67147729ccd77f4d8e85e8f9
piegowatapanna <klaudia.frankowska.lodz@gmail.com>
2025-08-08 19:18:33 +0200

    Dodanie stanu na listę notatek i ich wyświetlania

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,4 +1,4 @@
-import React, { useState }from 'react';
+import React, { useState } from 'react';
 
 //Komponent notes - w tej wersji posiada tylko pole do wpisania tekstu i przycisk Dodaj notatke
 export default function Notes () {
@@ -8,13 +8,30 @@ export default function Notes () {
   //setNoteText - fukncja do zmiany tej wartości
   const [noteText, setNoteText] = useState('');
 
+  //Stan dla wszystkich notatek
+  const [allNotes, setAllNotes] = useState([]);
+
+  //Funkcja obsługująca dodanie nowej notatki 
+  const handleAddNote = (event) => {
+    event.preventDefault(); // blokuje domyślne przeładowanie strony
+
+    const CleanText = noteText.trim(); //usuwamy spacje z początku/końca
+    if (!CleanText) return; // jeśli puste kończymy
+
+    //dodaj nową notatkę na początek listy 
+    setAllNotes ((prevNotes) => [CleanText, ...prevNotes]);
+
+    //wyczyść pole tekstowe 
+    setNoteText('');
+  };
+
   return (
     <div>
       {/*Tytuł podstrony */}
       <h1>Notes</h1>
 
       {/* Formularz dodawania notatki, e.preventDefault - blokuje domyślnie przeładowania strony po wysłaniu formularza*/}
-      <form onSubmit={(e) => e.preventDefault()}>
+      <form onSubmit={handleAddNote}>
         {/* Pole tekstowe kontrolowane przez React (wartość pochodzi ze stanu noteText) */}
         <input
         type="text"
@@ -25,6 +42,13 @@ export default function Notes () {
          {/* Przycisk do wysyłania formularza */}
         <button type='submit'>Dodaj notatkę</button>
       </form>
+
+      <ul>
+        {allNotes.length === 0 && <li>Brak notatek</li>}
+        {allNotes.map((note, index) => (
+          <li key={index}>{note}</li>
+        ))}
+      </ul>
       </div>
   );
 }
