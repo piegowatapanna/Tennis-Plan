a2862a514e0a330e3f3ea1fdccfef7127e3cef0a
piegowatapanna <klaudia.frankowska.lodz@gmail.com>
2025-08-09 12:40:46 +0200

    Dodanie podstawowej struktury komponentu Calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import './Calendar.css';
+import { Link } from 'react-router-dom';
 
-const Calendar = () => {
+export default function Calendar () {
+  const year = new Date().getFullYear();
+  const monthIndex = new Date().getMonth();
+  const daysInMonth = new Date(year, monthIndex + 1, 0).getDate();
+
+
   return (
     <div className="calendar-container">
       <h1 className='calendar-title'>Kalendarz</h1>
-    </div>
-  )
-}
+      <Link to="/" className='back-button'></Link>
+      <div className="calendar-grid">
+     {Array.from({ length: daysInMonth }, (_, i) => i + 1).map(day => (
+      <div key={day} className="calendar-day">
+       <span className="day-number">{day}</span>
+  </div>
+))}
+  </div>
 
-export default Calendar
+  <p className='calendar-placeholder'>
+  </p>
+  </div>
+  );
+}
